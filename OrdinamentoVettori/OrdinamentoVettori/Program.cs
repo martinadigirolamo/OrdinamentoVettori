e079dfb6f27719213fa6554ebb6af6670b9e19bc
Martina Di Girolamo <MARTINA.DI GIROLAMO@local.ittcesena.it>
2021-10-08 08:35:14 +0200

    Aggiunta commenti

--- OrdinamentoVettori/OrdinamentoVettori/Program.cs
+++ OrdinamentoVettori/OrdinamentoVettori/Program.cs
@@ -6,17 +6,22 @@ namespace OrdinamentoVettori
     {
         static void Main(string[] args)
         {
+
+            //creazione del vettore
             int [] vettore = new int [10] { 11, 55, -9, 7, 74, 147, -87, 6, 0, 18};
 
-
+            //richiamo al metodo di ordinamento
             ordina(vettore);
 
+            //ciclo per stampare a video il contenuto del vettore
             for (int i = 0; i < 10; i++)
                 Console.WriteLine(vettore[i]);
 
             Console.ReadKey();
         }
 
+
+        //metodo per ordinamento array
         static void ordina (int[] vettore)
         {
             Array.Sort(vettore);
